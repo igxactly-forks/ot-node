c4682d893f147aa492b869fb41271760844af9e9
UrosK <kukic.uros@yahoo.com>
2018-06-27 00:37:47 +0200

    Adds individual getters and setters to escorw as well as the strct definition for litigation in storage smart contract

--- modules/Blockchain/Ethereum/contracts/Storage.sol
+++ modules/Blockchain/Ethereum/contracts/Storage.sol
@@ -503,6 +503,42 @@ contract StorageContract {
 		EscrowStatus escrow_status;
 	}
 	mapping(bytes32 => mapping(address => EscrowDefinition)) public escrow; // escrow[import_id][DH_wallet]
+	function getEscrow.DC_wallet(bytes32 import_id, address DH_wallet) public view returns(address){
+		return escrow[import_id][DH_wallet].DC_wallet;
+	}
+	function getEscrow.token_amount(bytes32 import_id, address DH_wallet) public view returns(uint){
+		return escrow[import_id][DH_wallet].token_amount;
+	}
+	function getEscrow.tokens_sent(bytes32 import_id, address DH_wallet) public view returns(uint){
+		return escrow[import_id][DH_wallet].tokens_sent;
+	}
+	function getEscrow.stake_amount(bytes32 import_id, address DH_wallet) public view returns(uint){
+		return escrow[import_id][DH_wallet].stake_amount;
+	}
+	function getEscrow.last_confirmation_time(bytes32 import_id, address DH_wallet) public view returns(uint){
+		return escrow[import_id][DH_wallet].last_confirmation_time;
+	}
+	function getEscrow.end_time(bytes32 import_id, address DH_wallet) public view returns(uint){
+		return escrow[import_id][DH_wallet].end_time;
+	}
+	function getEscrow.total_time_in_seconds(bytes32 import_id, address DH_wallet) public view returns(uint){
+		return escrow[import_id][DH_wallet].total_time_in_seconds;
+	}
+	function getEscrow.litigation_interval_in_minutes(bytes32 import_id, address DH_wallet) public view returns(uint){
+		return escrow[import_id][DH_wallet].litigation_interval_in_minutes;
+	}
+	function getEscrow.litigation_root_hash(bytes32 import_id, address DH_wallet) public view returns(bytes32){
+		return escrow[import_id][DH_wallet].litigation_root_hash;
+	}
+	function getEscrow.distribution_root_hash(bytes32 import_id, address DH_wallet) public view returns(bytes32){
+		return escrow[import_id][DH_wallet].distribution_root_hash;
+	}
+	function getEscrow.checksum(bytes32 import_id, address DH_wallet) public view returns(uint){
+		return escrow[import_id][DH_wallet].checksum;
+	}
+	function getEscrow.escrow_status(bytes32 import_id, address DH_wallet) public view returns(EscrowStatus){
+		return escrow[import_id][DH_wallet].escrow_status;
+	}
 	function setEscrow(
 		bytes32 import_id,
 		address DH_wallet,
@@ -553,6 +589,68 @@ contract StorageContract {
 
 		emit EscrowChange(import_id, DH_wallet);
 	}
+	function setEscrow.DC_wallet(bytes32 import_id, address DH_wallet, uint DC_wallet) public onlyContracts{
+		if(escrow[import_id][DH_wallet].DC_wallet != DC_wallet)
+		escrow[import_id][DH_wallet].DC_wallet = DC_wallet;
+	}
+	function setEscrow.token_amount(bytes32 import_id, address DH_wallet, uint token_amount) public onlyContracts{
+		if(escrow[import_id][DH_wallet].token_amount != token_amount)
+		escrow[import_id][DH_wallet].token_amount = token_amount;
+	}
+	function setEscrow.tokens_sent(bytes32 import_id, address DH_wallet, uint tokens_sent) public onlyContracts{
+		if(escrow[import_id][DH_wallet].tokens_sent != tokens_sent)
+		escrow[import_id][DH_wallet].tokens_sent = tokens_sent;
+	}
+	function setEscrow.stake_amount(bytes32 import_id, address DH_wallet, uint stake_amount) public onlyContracts{
+		if(escrow[import_id][DH_wallet].stake_amount != stake_amount)
+		escrow[import_id][DH_wallet].stake_amount = stake_amount;
+	}
+	function setEscrow.last_confirmation_time(bytes32 import_id, address DH_wallet, uint last_confirmation_time) public onlyContracts{
+		if(escrow[import_id][DH_wallet].last_confirmation_time != last_confirmation_time)
+		escrow[import_id][DH_wallet].last_confirmation_time = last_confirmation_time;
+	}
+	function setEscrow.end_time(bytes32 import_id, address DH_wallet, uint end_time) public onlyContracts{
+		if(escrow[import_id][DH_wallet].end_time != end_time)
+		escrow[import_id][DH_wallet].end_time = end_time;
+	}
+	function setEscrow.total_time_in_seconds(bytes32 import_id, address DH_wallet, uint256 total_time_in_seconds) public onlyContracts{
+	if(escrow[import_id][DH_wallet].total_time_in_seconds != total_time_in_seconds)
+		escrow[import_id][DH_wallet].total_time_in_seconds = total_time_in_seconds;
+	}
+	function setEscrow.litigation_interval_in_minutes(bytes32 import_id, address DH_wallet, uint256 litigation_interval_in_minutes) public onlyContracts{
+		if(escrow[import_id][DH_wallet].litigation_interval_in_minutes != litigation_interval_in_minutes)
+		escrow[import_id][DH_wallet].litigation_interval_in_minutes = litigation_interval_in_minutes;
+	}
+	function setEscrow.litigation_root_hash(bytes32 import_id, address DH_wallet, bytes32 litigation_root_hash) public onlyContracts{
+		if(escrow[import_id][DH_wallet].litigation_root_hash != litigation_root_hash)
+		escrow[import_id][DH_wallet].litigation_root_hash = litigation_root_hash;
+	}
+	function setEscrow.distribution_root_hash(bytes32 import_id, address DH_wallet, bytes32 distribution_root_hash) public onlyContracts{
+		if(escrow[import_id][DH_wallet].distribution_root_hash != distribution_root_hash)
+		escrow[import_id][DH_wallet].distribution_root_hash = distribution_root_hash;
+	}
+	function setEscrow.checksum(bytes32 import_id, address DH_walle, uint256 checksum) public onlyContracts{
+		if(escrow[import_id][DH_wallet].checksum != checksum)
+		escrow[import_id][DH_wallet].checksum = checksum;
+	}
+	function setEscrow.escrow_status(bytes32 import_id, address DH_wallet, EscrowStatus escrow_status) public onlyContracts{
+		if(escrow[import_id][DH_wallet].escrow_status != escrow_status)
+		escrow[import_id][DH_wallet].escrow_status = escrow_status;
+	}
+
+
+	enum LitigationStatus {inactive, initiated, answered, timed_out, completed}
+
+ 	struct LitigationDefinition{
+ 		uint requested_data_index;
+ 		bytes32 requested_data;
+ 		bytes32[] hash_array;
+ 		uint litigation_start_time;
+ 		uint answer_timestamp;
+ 		LitigationStatus litigation_status;
+ 	}
+
+ 	mapping(bytes32 => mapping ( address => LitigationDefinition)) public litigation;
 
 
 	struct PurchasedDataDefinition {
