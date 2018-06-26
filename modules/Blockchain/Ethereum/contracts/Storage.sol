b13a054bb08ab37debc551c2314cb079ae0df23b
UrosK <kukic.uros@yahoo.com>
2018-06-27 00:57:03 +0200

    Fixes compilation errors in storage smmart contract

--- modules/Blockchain/Ethereum/contracts/Storage.sol
+++ modules/Blockchain/Ethereum/contracts/Storage.sol
@@ -503,40 +503,40 @@ contract StorageContract {
 		EscrowStatus escrow_status;
 	}
 	mapping(bytes32 => mapping(address => EscrowDefinition)) public escrow; // escrow[import_id][DH_wallet]
-	function getEscrow.DC_wallet(bytes32 import_id, address DH_wallet) public view returns(address){
+	function getEscrow_DC_wallet(bytes32 import_id, address DH_wallet) public view returns(address){
 		return escrow[import_id][DH_wallet].DC_wallet;
 	}
-	function getEscrow.token_amount(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getEscrow_token_amount(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return escrow[import_id][DH_wallet].token_amount;
 	}
-	function getEscrow.tokens_sent(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getEscrow_tokens_sent(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return escrow[import_id][DH_wallet].tokens_sent;
 	}
-	function getEscrow.stake_amount(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getEscrow_stake_amount(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return escrow[import_id][DH_wallet].stake_amount;
 	}
-	function getEscrow.last_confirmation_time(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getEscrow_last_confirmation_time(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return escrow[import_id][DH_wallet].last_confirmation_time;
 	}
-	function getEscrow.end_time(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getEscrow_end_time(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return escrow[import_id][DH_wallet].end_time;
 	}
-	function getEscrow.total_time_in_seconds(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getEscrow_total_time_in_seconds(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return escrow[import_id][DH_wallet].total_time_in_seconds;
 	}
-	function getEscrow.litigation_interval_in_minutes(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getEscrow_litigation_interval_in_minutes(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return escrow[import_id][DH_wallet].litigation_interval_in_minutes;
 	}
-	function getEscrow.litigation_root_hash(bytes32 import_id, address DH_wallet) public view returns(bytes32){
+	function getEscrow_litigation_root_hash(bytes32 import_id, address DH_wallet) public view returns(bytes32){
 		return escrow[import_id][DH_wallet].litigation_root_hash;
 	}
-	function getEscrow.distribution_root_hash(bytes32 import_id, address DH_wallet) public view returns(bytes32){
+	function getEscrow_distribution_root_hash(bytes32 import_id, address DH_wallet) public view returns(bytes32){
 		return escrow[import_id][DH_wallet].distribution_root_hash;
 	}
-	function getEscrow.checksum(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getEscrow_checksum(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return escrow[import_id][DH_wallet].checksum;
 	}
-	function getEscrow.escrow_status(bytes32 import_id, address DH_wallet) public view returns(EscrowStatus){
+	function getEscrow_escrow_status(bytes32 import_id, address DH_wallet) public view returns(EscrowStatus){
 		return escrow[import_id][DH_wallet].escrow_status;
 	}
 	function setEscrow(
@@ -589,51 +589,51 @@ contract StorageContract {
 
 		emit EscrowChange(import_id, DH_wallet);
 	}
-	function setEscrow.DC_wallet(bytes32 import_id, address DH_wallet, uint DC_wallet) public onlyContracts{
+	function setEscrow_DC_wallet(bytes32 import_id, address DH_wallet, address DC_wallet) public onlyContracts{
 		if(escrow[import_id][DH_wallet].DC_wallet != DC_wallet)
 		escrow[import_id][DH_wallet].DC_wallet = DC_wallet;
 	}
-	function setEscrow.token_amount(bytes32 import_id, address DH_wallet, uint token_amount) public onlyContracts{
+	function setEscrow_token_amount(bytes32 import_id, address DH_wallet, uint token_amount) public onlyContracts{
 		if(escrow[import_id][DH_wallet].token_amount != token_amount)
 		escrow[import_id][DH_wallet].token_amount = token_amount;
 	}
-	function setEscrow.tokens_sent(bytes32 import_id, address DH_wallet, uint tokens_sent) public onlyContracts{
+	function setEscrow_tokens_sent(bytes32 import_id, address DH_wallet, uint tokens_sent) public onlyContracts{
 		if(escrow[import_id][DH_wallet].tokens_sent != tokens_sent)
 		escrow[import_id][DH_wallet].tokens_sent = tokens_sent;
 	}
-	function setEscrow.stake_amount(bytes32 import_id, address DH_wallet, uint stake_amount) public onlyContracts{
+	function setEscrow_stake_amount(bytes32 import_id, address DH_wallet, uint stake_amount) public onlyContracts{
 		if(escrow[import_id][DH_wallet].stake_amount != stake_amount)
 		escrow[import_id][DH_wallet].stake_amount = stake_amount;
 	}
-	function setEscrow.last_confirmation_time(bytes32 import_id, address DH_wallet, uint last_confirmation_time) public onlyContracts{
+	function setEscrow_last_confirmation_time(bytes32 import_id, address DH_wallet, uint last_confirmation_time) public onlyContracts{
 		if(escrow[import_id][DH_wallet].last_confirmation_time != last_confirmation_time)
 		escrow[import_id][DH_wallet].last_confirmation_time = last_confirmation_time;
 	}
-	function setEscrow.end_time(bytes32 import_id, address DH_wallet, uint end_time) public onlyContracts{
+	function setEscrow_end_time(bytes32 import_id, address DH_wallet, uint end_time) public onlyContracts{
 		if(escrow[import_id][DH_wallet].end_time != end_time)
 		escrow[import_id][DH_wallet].end_time = end_time;
 	}
-	function setEscrow.total_time_in_seconds(bytes32 import_id, address DH_wallet, uint256 total_time_in_seconds) public onlyContracts{
+	function setEscrow_total_time_in_seconds(bytes32 import_id, address DH_wallet, uint256 total_time_in_seconds) public onlyContracts{
 		if(escrow[import_id][DH_wallet].total_time_in_seconds != total_time_in_seconds)
 		escrow[import_id][DH_wallet].total_time_in_seconds = total_time_in_seconds;
 	}
-	function setEscrow.litigation_interval_in_minutes(bytes32 import_id, address DH_wallet, uint256 litigation_interval_in_minutes) public onlyContracts{
+	function setEscrow_litigation_interval_in_minutes(bytes32 import_id, address DH_wallet, uint256 litigation_interval_in_minutes) public onlyContracts{
 		if(escrow[import_id][DH_wallet].litigation_interval_in_minutes != litigation_interval_in_minutes)
 		escrow[import_id][DH_wallet].litigation_interval_in_minutes = litigation_interval_in_minutes;
 	}
-	function setEscrow.litigation_root_hash(bytes32 import_id, address DH_wallet, bytes32 litigation_root_hash) public onlyContracts{
+	function setEscrow_litigation_root_hash(bytes32 import_id, address DH_wallet, bytes32 litigation_root_hash) public onlyContracts{
 		if(escrow[import_id][DH_wallet].litigation_root_hash != litigation_root_hash)
 		escrow[import_id][DH_wallet].litigation_root_hash = litigation_root_hash;
 	}
-	function setEscrow.distribution_root_hash(bytes32 import_id, address DH_wallet, bytes32 distribution_root_hash) public onlyContracts{
+	function setEscrow_distribution_root_hash(bytes32 import_id, address DH_wallet, bytes32 distribution_root_hash) public onlyContracts{
 		if(escrow[import_id][DH_wallet].distribution_root_hash != distribution_root_hash)
 		escrow[import_id][DH_wallet].distribution_root_hash = distribution_root_hash;
 	}
-	function setEscrow.checksum(bytes32 import_id, address DH_walle, uint256 checksum) public onlyContracts{
+	function setEscrow_checksum(bytes32 import_id, address DH_wallet, uint256 checksum) public onlyContracts{
 		if(escrow[import_id][DH_wallet].checksum != checksum)
 		escrow[import_id][DH_wallet].checksum = checksum;
 	}
-	function setEscrow.escrow_status(bytes32 import_id, address DH_wallet, EscrowStatus escrow_status) public onlyContracts{
+	function setEscrow_escrow_status(bytes32 import_id, address DH_wallet, EscrowStatus escrow_status) public onlyContracts{
 		if(escrow[import_id][DH_wallet].escrow_status != escrow_status)
 		escrow[import_id][DH_wallet].escrow_status = escrow_status;
 	}
@@ -649,46 +649,46 @@ contract StorageContract {
 		LitigationStatus litigation_status;
 	}
 	mapping(bytes32 => mapping ( address => LitigationDefinition)) public litigation; // litigation[import_id][DH_wallet]
-	function getLitigation.requested_data_index(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getLitigation_requested_data_index(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return litigation[import_id][DH_wallet].requested_data_index;
 	}
-	function getLitigation.requested_data(bytes32 import_id, address DH_wallet) public view returns(bytes32){
-		return litigation[import_id][DH_wallet].requested_data_index;
+	function getLitigation_requested_data(bytes32 import_id, address DH_wallet) public view returns(bytes32){
+		return litigation[import_id][DH_wallet].requested_data;
 	}
-	function getLitigation.hash_array(bytes32 import_id, address DH_wallet) public view returns(bytes32[]){
+	function getLitigation_hash_array(bytes32 import_id, address DH_wallet) public view returns(bytes32[]){
 		return litigation[import_id][DH_wallet].hash_array;
 	}
-	function getLitigation.litigation_start_time(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getLitigation_litigation_start_time(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return litigation[import_id][DH_wallet].litigation_start_time;
 	}
-	function getLitigation.answer_timestamp(bytes32 import_id, address DH_wallet) public view returns(uint){
+	function getLitigation_answer_timestamp(bytes32 import_id, address DH_wallet) public view returns(uint){
 		return litigation[import_id][DH_wallet].answer_timestamp;
 	}
-	function getLitigation.litigation_status(bytes32 import_id, address DH_wallet) public view returns(LitigationStatus){
+	function getLitigation_litigation_status(bytes32 import_id, address DH_wallet) public view returns(LitigationStatus){
 		return litigation[import_id][DH_wallet].litigation_status;
 	}
 
-	function setLitigation.requested_data_index(bytes32 import_id, address DH_wallet, uint requested_data_index) public onlyContracts{
+	function setLitigation_requested_data_index(bytes32 import_id, address DH_wallet, uint requested_data_index) public onlyContracts{
 		if(litigation[import_id][DH_wallet].requested_data_index != requested_data_index)
 		litigation[import_id][DH_wallet].requested_data_index = requested_data_index;
 	} 	
-	function setLitigation.requested_data(bytes32 import_id, address DH_wallet, bytes32 requested_data) public onlyContracts{
+	function setLitigation_requested_data(bytes32 import_id, address DH_wallet, bytes32 requested_data) public onlyContracts{
 		if(litigation[import_id][DH_wallet].requested_data != requested_data)
 		litigation[import_id][DH_wallet].requested_data = requested_data;
 	}
-	function setLitigation.hash_array(bytes32 import_id, address DH_wallet, bytes32[] hash_array) public onlyContracts{
-		if(litigation[import_id][DH_wallet].hash_array != hash_array)
+	function setLitigation_hash_array(bytes32 import_id, address DH_wallet, bytes32[] hash_array) public onlyContracts{
+		// if(litigation[import_id][DH_wallet].hash_array != hash_array)
 		litigation[import_id][DH_wallet].hash_array = hash_array;
 	}
-	function setLitigation.litigation_start_time(bytes32 import_id, address DH_wallet, uint litigation_start_time) public onlyContracts{
+	function setLitigation_litigation_start_time(bytes32 import_id, address DH_wallet, uint litigation_start_time) public onlyContracts{
 		if(litigation[import_id][DH_wallet].litigation_start_time != litigation_start_time)
 		litigation[import_id][DH_wallet].litigation_start_time = litigation_start_time;
 	}
-	function setLitigation.answer_timestamp(bytes32 import_id, address DH_wallet, uint answer_timestamp) public onlyContracts{
+	function setLitigation_answer_timestamp(bytes32 import_id, address DH_wallet, uint answer_timestamp) public onlyContracts{
 		if(litigation[import_id][DH_wallet].answer_timestamp != answer_timestamp)
 		litigation[import_id][DH_wallet].answer_timestamp = answer_timestamp;
 	}
-	function setLitigation.litigation_status(bytes32 import_id, address DH_wallet, LitigationStatus litigation_status) public onlyContracts{
+	function setLitigation_litigation_status(bytes32 import_id, address DH_wallet, LitigationStatus litigation_status) public onlyContracts{
 		if(litigation[import_id][DH_wallet].litigation_status != litigation_status)
 		litigation[import_id][DH_wallet].litigation_status = litigation_status;
 	}
