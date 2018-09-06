90a63cee11a20b82643462ff44c6249896d8032d
Uros Kukic <kukic.uros@yahoo.com>
2018-09-06 10:54:57 +0200

    Organize truffle SC deployment script

--- modules/Blockchain/Ethereum/migrations/2_total_migration.js
+++ modules/Blockchain/Ethereum/migrations/2_total_migration.js
@@ -48,28 +48,24 @@ module.exports = async (deployer, network, accounts) => {
     const recepients = [];
 
     switch (network) {
+    case 'test':
+        await deployer.deploy(TestingUtilities);
     case 'ganache':
         await deployer.deploy(ContractHub).then((result) => hub = result);
-        // console.log(hub.address);
 
         profileStorage = await deployer.deploy(ProfileStorage, hub.address, { gas: 6000000, from: accounts[0] });
-        // console.log(profileStorage.address);
         await hub.setProfileStorageAddress(profileStorage.address);
 
         biddingStorage = await deployer.deploy(BiddingStorage, hub.address, { gas: 6000000, from: accounts[0] });
-        // console.log(biddingStorage.address);
         await hub.setBiddingStorageAddress(biddingStorage.address);
 
         escrowStorage = await deployer.deploy(EscrowStorage, hub.address, { gas: 6000000, from: accounts[0] });
-        // console.log(escrowStorage.address)
         await hub.setEscrowStorageAddress(escrowStorage.address);
 
         litigationStorage = await deployer.deploy(LitigationStorage, hub.address, { gas: 6000000, from: accounts[0] });
-        // console.log(litigationStorage.address);
         await hub.setLitigationStorageAddress(litigationStorage.address);
 
         readingStorage = await deployer.deploy(ReadingStorage, hub.address, { gas: 6000000, from: accounts[0] });
-        // console.log(readingStorage.address);
         await hub.setReadingStorageAddress(readingStorage.address, { from: accounts[0] });
 
 
@@ -105,119 +101,6 @@ module.exports = async (deployer, network, accounts) => {
         await token.mintMany(recepients, amounts, { from: accounts[0] });
         await token.finishMinting({ from: accounts[0] });
 
-        break;
-    case 'test':
-            deployer.deploy(TestingUtilities);
-            deployer.deploy(ContractHub).then((result) => {
-            hub = result;
-            deployer.deploy(ProfileStorage, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            profileStorage = result;
-            // console.log(profileStorage.address);
-            hub.setProfileStorageAddress(profileStorage.address)
-        .then(() => {
-            deployer.deploy(BiddingStorage, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            biddingStorage = result;
-            // console.log(biddingStorage.address);
-            hub.setBiddingStorageAddress(biddingStorage.address)
-        .then(() => {
-            deployer.deploy(EscrowStorage, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            escrowStorage = result;
-            // console.log(escrowStorage.address)
-            hub.setEscrowStorageAddress(escrowStorage.address)
-        .then(() => {
-            deployer.deploy(LitigationStorage, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            litigationStorage = result;
-            // console.log(litigationStorage.address);
-            hub.setLitigationStorageAddress(litigationStorage.address)
-        .then(() => {
-            deployer.deploy(ReadingStorage, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            readingStorage = result;
-            // console.log(readingStorage.address);
-            hub.setReadingStorageAddress(readingStorage.address, { from: accounts[0] })
-        .then(() => {
-            deployer.deploy(TracToken, accounts[0], accounts[1], accounts[2])
-        .then((result) => {
-            token = result;
-            hub.setTokenAddress(token.address)
-        .then(() => {
-            deployer.deploy(OTFingerprintStore)
-        .then((result) => {
-            fingerprint = result;
-            hub.setFingerprintAddress(fingerprint.address)
-        .then(() => {
-            deployer.deploy(Profile, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            profile = result;
-            hub.setProfileAddress(profile.address)
-        .then(() => {
-            deployer.deploy(Bidding, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            bidding = result;
-            hub.setBiddingAddress(bidding.address)
-        .then(() => {
-            deployer.deploy(EscrowHolder, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            escrow = result;
-            hub.setEscrowAddress(escrow.address)
-        .then(() => {
-            deployer.deploy(Litigation, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            litigation = result;
-            hub.setLitigationAddress(litigation.address)
-        .then(() => {
-            deployer.deploy(Reading, hub.address, { gas: 6000000, from: accounts[0] })
-        .then((result) => {
-            reading = result;
-            hub.setReadingAddress(reading.address)
-        .then(() => {
-            bidding.initiate()
-        .then(() => {
-            escrow.initiate()
-        .then(() => {
-            litigation.initiate()
-        .then(() => {
-            for (let i = 0; i < 10; i += 1) {
-                amounts.push(amountToMint);
-                recepients.push(accounts[i]);
-            }
-            token.mintMany(recepients, amounts, { from: accounts[0] })
-        .then(() => {
-            token.finishMinting({ from: accounts[0] })
-        .then(() => console.log("Finished migrations"))
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
-        });
         break;
     case 'rinkeby':
     try{
